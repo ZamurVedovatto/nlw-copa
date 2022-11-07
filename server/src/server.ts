d185102ae565958a4ef20397b3c777657fcfb377
Zamur Borges Vedovatto <vedovatto@gmail.com>
2022-11-06 22:58:33 -0300

    polls count endpoint

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,12 +1,26 @@
 import Fastify from 'fastify'
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient({
+  log: ['query'],
+})
 
 async function bootstrap() {
   const fastify = Fastify({
     logger: true
   })
 
-  fastify.get('/polls/count', () => {
-    return { count: 123 }
+  fastify.get('/polls/count', async() => {
+    // const polls = await prisma.poll.findMany({
+    //   where: {
+    //     code: {
+    //       startsWith: 'M'
+    //     }
+    //   }
+    // })
+    const count = await prisma.poll.count()
+
+    return { count }
   })
 
   await fastify.listen({ port: 3333 })
