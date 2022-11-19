c445be99f0a1a290856563cf42d4e1d2b0ea8d6c
Zamur Borges Vedovatto <vedovatto@gmail.com>
2022-11-19 15:25:35 -0300

    db structure and relationships

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,35 +1,26 @@
-import Fastify from 'fastify'
-import cors from '@fastify/cors'
-import { PrismaClient } from '@prisma/client'
+import Fastify from "fastify";
+import cors from "@fastify/cors";
+import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient({
-  log: ['query'],
-})
+  log: ["query"],
+});
 
 async function bootstrap() {
   const fastify = Fastify({
-    logger: true
-  })
+    logger: true,
+  });
 
   await fastify.register(cors, {
-    origin: true
-  })
+    origin: true,
+  });
 
-  fastify.get('/polls/count', async() => {
-    // const polls = await prisma.poll.findMany({
-    //   where: {
-    //     code: {
-    //       startsWith: 'M'
-    //     }
-    //   }
-    // })
-    const count = await prisma.poll.count()
+  fastify.get("/polls/count", async () => {
+    const count = await prisma.poll.count();
+    return { count };
+  });
 
-    return { count }
-  })
-
-  // await fastify.listen({ port: 3333, host: '0.0.0.0' })
-  await fastify.listen({ port: 3333 })
+  await fastify.listen({ port: 3333 });
 }
 
-bootstrap()
+bootstrap();
