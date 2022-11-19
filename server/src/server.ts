917b5b1b63db1697a8c21d74743c11c7e408f9b6
Zamur Borges Vedovatto <vedovatto@gmail.com>
2022-11-19 17:58:20 -0300

    api routes

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,6 +1,8 @@
 import Fastify from "fastify";
 import cors from "@fastify/cors";
 import { PrismaClient } from "@prisma/client";
+import { z as zod } from "zod";
+import ShortUniqueId from "short-unique-id";
 
 const prisma = new PrismaClient({
   log: ["query"],
@@ -15,9 +17,35 @@ async function bootstrap() {
     origin: true,
   });
 
-  fastify.get("/polls/count", async () => {
+  fastify.get("/poll/count", async () => {
     const count = await prisma.poll.count();
     return { count };
+  });
+
+  fastify.get("/user/count", async () => {
+    const count = await prisma.user.count();
+    return { count };
+  });
+
+  fastify.get("/guess/count", async () => {
+    const count = await prisma.guess.count();
+    return { count };
+  });
+
+  fastify.post("/poll", async (request, reply) => {
+    const createPollBody = zod.object({
+      title: zod.string(),
+    });
+    const { title } = createPollBody.parse(request.body);
+    const generate = new ShortUniqueId({ length: 6 });
+    const code = String(generate()).toUpperCase();
+    await prisma.poll.create({
+      data: {
+        title,
+        code,
+      },
+    });
+    return reply.status(201).send({ code });
   });
 
   await fastify.listen({ port: 3333 });
