8c362cebd6d3d889fb0b9c51bc556215781f8a7a
Zamur Borges Vedovatto <vedovatto@gmail.com>
2022-11-19 18:11:59 -0300

    web refactor

---
+++ web/src/pages/index.tsx
@@ -0,0 +1,18 @@
+interface HomeProps {
+  count: number;
+}
+
+export default function Home({ count }: HomeProps) {
+  return <h1>hello world {count}</h1>;
+}
+
+export const getServerSideProps = async () => {
+  const response = await fetch("http://localhost:3333/poll/count");
+  const data = await response.json();
+
+  return {
+    props: {
+      count: data.count,
+    },
+  };
+};
