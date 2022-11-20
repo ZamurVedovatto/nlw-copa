dbaa6747b0b0a164930e9f0f1928d5f94d29e193
Zamur Borges Vedovatto <vedovatto@gmail.com>
2022-11-20 18:57:57 -0300

    adjust

--- web/src/pages/ranking.tsx
+++ web/src/pages/ranking.tsx
@@ -7,46 +7,18 @@ interface RankingProps {
 
 const fakeData = [
   {
-    name: "Page A",
-    uv: 4000,
-    pv: 2400,
-    amt: 2400,
-  },
-  {
-    name: "Page B",
-    uv: 3000,
-    pv: 1398,
-    amt: 2210,
-  },
-  {
-    name: "Page C",
-    uv: 2000,
-    pv: 9800,
-    amt: 2290,
-  },
-  {
-    name: "Page D",
-    uv: 2780,
-    pv: 3908,
-    amt: 2000,
-  },
-  {
-    name: "Page E",
-    uv: 1890,
-    pv: 4800,
-    amt: 2181,
-  },
-  {
-    name: "Page F",
-    uv: 2390,
-    pv: 3800,
-    amt: 2500,
-  },
-  {
-    name: "Page G",
-    uv: 3490,
-    pv: 4300,
-    amt: 2100,
+    name: "1",
+    zamur: 7,
+    cabu: 7,
+    pato: 5,
+    tata: 5,
+    thomaz: 5,
+    dudu: 5,
+    luquete: 5,
+    saulim: 4,
+    andré: 0,
+    reinaldo: 0,
+    gober: 0,
   },
 ];
 
@@ -59,7 +31,6 @@ export default function Ranking({ count }): RankingProps {
 
   return (
     <div className="ranking">
-      <span>{count}</span>
       <Classification data={data} />
     </div>
   );
